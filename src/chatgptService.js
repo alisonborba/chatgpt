4fdd1163dba96d71c9bf4bd9a1e288459ec057c3
Alison Borba <alisonborbaa@gmail.com>
2023-03-21 18:11:26 +0000

    fetting a smaller text

--- src/chatgptService.js
+++ src/chatgptService.js
@@ -5,7 +5,7 @@ export default async function chatgptApi(text, person) {
     const apiKey = process.env.REACT_APP_CHATGPT_KEY
 
     const data = JSON.stringify({
-        prompt: `${text}. Fale como se fosse ${person}, termine o texto questionando ou perguntando algo e resuma em aproximadamente 75 palavras.`,
+        prompt: `${text}. Fale como se fosse ${person}, termine o texto questionando ou perguntando algo e resuma em aproximadamente 60 palavras.`,
         max_tokens: 200,
         n: 1,
         // stop: "."
