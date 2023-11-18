c7e12a39bc5c881108abbfb6a6ec2a3467f64838
alisonborba <alisonborbaa@gmail.com>
2023-11-18 13:51:00 +0000

    improving the speech service

--- src/services/speechService.ts
+++ src/services/speechService.ts
@@ -1,71 +1,47 @@
-let _speechSynth
-let _voices
-const _cache = {}
+let speechSynth = window.speechSynthesis;
+let voices: SpeechSynthesisVoice[];
+let cachedVoice: SpeechSynthesisVoice | null;
 
 /**
- * retries until there have been voices loaded. No stopper flag included in this example. 
+ * retries until there have been voices loaded. No stopper flag included in this example.
  * Note that this function assumes, that there are voices installed on the host system.
  */
 
-const loadVoicesWhenAvailable = (onComplete = () => {}) => {
-  _speechSynth = window.speechSynthesis
-  const voices = _speechSynth.getVoices()
-
-  if (voices.length !== 0) {
-    _voices = voices
-    onComplete()
-  } else {
-    return setTimeout(function () { loadVoicesWhenAvailable(onComplete) }, 100)
-  }
-}
+// on document ready
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    voices = speechSynth.getVoices();
+    console.log("voices", voices);
+  });
+});
 
 /**
  * Returns the first found voice for a given language code.
  */
-
-const getVoices = (locale) => {
-  if (!_speechSynth) {
-    throw new Error('Browser does not support speech synthesis')
-  }
-  if (_cache[locale]) return _cache[locale]
-
-  _cache[locale] = _voices.filter(voice => voice.lang === locale)
-  return _cache[locale]
-}
-
-/**
- * Speak a certain text 
- * @param locale the locale this voice requires
- * @param text the text to speak
- * @param onEnd callback if tts is finished
- */
-
-function playByText (locale, text, onEnd) {
-  const voices = getVoices(locale)
-  console.log('voices', voices)
-
-  // TODO load preference here, e.g. male / female etc.
-  // TODO but for now we just use the first occurrence
-  const utterance = new window.SpeechSynthesisUtterance()
-  utterance.voice = voices[0]
-  utterance.voiceURI = 'native'
-  utterance.volume = 1
-  utterance.text = text
-  utterance.lang = locale
-
-  if (onEnd) {
-    utterance.onend = onEnd
+const getVoices = (locale: string) => {
+  if (!speechSynth) {
+    throw new Error("Browser does not support speech synthesis");
   }
 
-  _speechSynth.cancel() // cancel current speak, if any is running
-  _speechSynth.speak(utterance)
-}
+  if (cachedVoice) return cachedVoice;
 
-// on document ready
-loadVoicesWhenAvailable(() => { console.log("loadVoicesWhenAvailable") })
+  cachedVoice = voices.find((v) => v.lang === locale) || null;
+  return cachedVoice;
+};
 
-const speak = (locale, text) => {
-  setTimeout(() => playByText(locale, text), 1)
-}
+const speak = (locale: string, text: string) => {
+  const selectedVoice = getVoices(locale);
+
+  // @TODO load preference here, e.g. male / female etc.
+  // @TODO but for now we just use the first occurrence
+  const utterance = new window.SpeechSynthesisUtterance();
+  utterance.voice = selectedVoice;
+  utterance.volume = 1;
+  utterance.text = text;
+  utterance.lang = locale;
+
+  speechSynth.cancel(); // cancel current speak, if any is running
+  speechSynth.speak(utterance);
+};
 
 export default speak;
